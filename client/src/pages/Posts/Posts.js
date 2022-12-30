5dc302fcd90c24e4fbfb2012804b049a22b6ad33
Alejandra Cáceres <alejandraicaceres@gmail.com>
2022-12-30 09:46:45 -0300

    Refactoring folders - integrate client and server

---
+++ client/src/pages/Posts/Posts.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Post from "../../components/Post/Post"
+import { gql, useQuery } from "@apollo/client"
+
+const GET_POSTS = gql`
+	query {
+		posts {
+			id
+			title
+			content
+			createdAt
+			user {
+				name
+			}
+		}
+	}
+`
+
+export default function Posts() {
+	const { data, error, loading } = useQuery(GET_POSTS)
+
+	if (error) return <div>Error </div>
+	if (loading) return <div>loading...</div>
+
+	const { posts } = data
+
+	return (
+		<div>
+			{posts.map((post) => {
+				return (
+					<Post
+						key={post.id}
+						title={post.title}
+						content={post.content}
+						date={post.createdAt}
+						id={post.id}
+						user={post.user.name}
+					/>
+				)
+			})}
+		</div>
+	)
+}
---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,68 @@
+import React from "react"
+import { useParams } from "react-router"
+import AddPostModal from "../../components/AddPostModal/AddPostModal"
+import Post from "../../components/Post/Post"
+import { gql, useQuery } from "@apollo/client"
+
+const GET_PROFILE = gql`
+	query GetProfile($userId: ID!) {
+		profile(userId: $userId) {
+			bio
+			user {
+				id
+				name
+				posts {
+					id
+					title
+					content
+					createdAt
+				}
+			}
+		}
+	}
+`
+
+export default function Profile() {
+	const { id } = useParams()
+
+	const { data, error, loading } = useQuery(GET_PROFILE, {
+		variables: { userId: id },
+	})
+
+	if (error) return <div>Error </div>
+	if (loading) return <div>loading...</div>
+
+	const { profile } = data
+
+	return (
+		<div>
+			<div
+				style={{
+					marginBottom: "2rem",
+					display: "flex ",
+					justifyContent: "space-between",
+				}}
+			>
+				<div>
+					<h1>{profile.user.name}</h1>
+					<p>{profile.user.bio}</p>
+				</div>
+				<div>{"profile" ? <AddPostModal /> : null}</div>
+			</div>
+			<div>
+				{profile.user.posts.map((post) => {
+					return (
+						<Post
+							key={post.id}
+							title={post.title}
+							content={post.content}
+							date={post.createdAt}
+							id={post.id}
+							user={profile.user.name}
+						/>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
